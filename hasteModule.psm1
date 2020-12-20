d6ffd81077f84899c745467b7e62cc79f7d995ec
aleybe <alexbennettece@gmail.com>
2020-12-20 08:23:27 +0800

    update

---
+++ hasteModule.psm1
@@ -0,0 +1,68 @@
+function New-Haste {
+
+    [Alias("hp")]
+    [Alias("haste")]
+    [Alias("shh")]
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true
+        )]
+        [array]$InputValue,
+        $CopyUrlToClipboard = $false
+    )
+
+    begin {
+        [System.Collections.ArrayList]$dataArray = @()
+    }
+
+    process {
+        $dataArray.Add($InputValue) >> $null
+    }
+
+    end {
+
+        $uri = "https://hastebin.com/documents"
+        $body = ($dataArray | Out-String).TrimEnd()
+
+        $result = Invoke-RestMethod -Method Post -Body $body -Uri $uri
+        $key = $result.key
+
+        $returnable = "https://hastebin.com/$key"
+
+        if ($CopyUrlToClipboard -eq $true) {
+            Set-Clipboard $returnable
+            return "$returnable copied to clipboard!"
+        } else {
+            return $returnable
+        }
+    }
+}
+
+function New-HasteWithCopy {
+    [Alias("hastec")]
+    [Alias("shc")]
+    [Alias("yy")]
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true
+        )]
+        [array]$InputValue
+    )
+
+    begin {
+        [System.Collections.ArrayList]$dataArray = @()
+    }
+
+    process {
+        $dataArray.Add($InputValue) >> $null
+    }
+
+    end {
+        $covertedString = $dataArray | Out-String
+        Set-Haste $covertedString -CopyUrlToClipboard $true
+    }
+}
